7cc933e680bd1a32d24bbf57610257281e27aee1
seotaeyang <seotaeyang@naver.com>
2023-12-20 20:14:30 +0900

    [ver.3]직진/회전기능 개선, Firebase 기본사항 추가
    1.ForwardAction
    skidJoy 에 들어갈 속도를 durationMillis 라는 변수를 이용하여 짧은 시간동안엔 짧게, 긴 시간동안엔 길게 이동하는 클래스로 만듬.
    2. RobotController
    Firebase를 이용할 기본사항 추가.(angle, distance, out, time 데이터)
    Queue를 이용해 time에 따라 순서대로 프로그램 이행.
    OnDetectionStateChangedListener 의 state를 이용하여 장애물(Temi, 컬링핀)을 만나면 회전하고 직진하는 기능 추가

--- app/src/main/java/com/example/temicurling/ForwardAction.java
+++ app/src/main/java/com/example/temicurling/ForwardAction.java
@@ -1,47 +1,36 @@
 package com.example.temicurling;
 
-
-import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
-
 import com.robotemi.sdk.Robot;
+import com.robotemi.sdk.listeners.OnDetectionStateChangedListener;
+
+
 public class ForwardAction {
-    private static Handler handler;
-    private static Robot robot;
+    private Handler handler;
+    private Robot robot;
+    private float currentSpeed = 1.0F;
 
     public ForwardAction(Handler handler, Robot robot) {
         this.handler = handler;
         this.robot = robot;
     }
 
-    public static void moveForwardForDuration(int durationMillis) {
-        // 500ms 간격으로 작업을 반복하기 위한 Runnable
+    public void moveForwardForDuration(int durationMillis) {
         Runnable skidJoyRunnable = new Runnable() {
-            float k = 1;
-
             @Override
             public void run() {
-                // run 반복 시 a만큼 감소
                 float a = (0.5F / durationMillis * 1000);
+                robot.skidJoy(currentSpeed, 0);
+                currentSpeed = currentSpeed - a;
 
-                robot.skidJoy(k, 0);
-                k = k - a;
-
-                // 500ms 후에 다시 실행
-                handler.postDelayed(this, 500);
+                if (currentSpeed > 0) {
+                    handler.postDelayed(this, 500);
+                }
             }
         };
 
-        // 500ms 후에 직진을 시작
         handler.postDelayed(skidJoyRunnable, 500);
 
-        // 직진 완료까지 대기
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                handler.removeCallbacks(skidJoyRunnable);
-            }
-        }, durationMillis);
+        handler.postDelayed(() -> handler.removeCallbacks(skidJoyRunnable), durationMillis);
     }
 }
--- app/src/main/java/com/example/temicurling/RobotController.java
+++ app/src/main/java/com/example/temicurling/RobotController.java
@@ -1,49 +1,170 @@
 package com.example.temicurling;
 
+import android.os.Build;
 import android.os.Handler;
+import android.util.Log;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
 import com.robotemi.sdk.Robot;
 import com.robotemi.sdk.navigation.model.SpeedLevel;
+import com.robotemi.sdk.navigation.model.*;
+import com.robotemi.sdk.listeners.OnDetectionStateChangedListener;
 
-public class RobotController {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class RobotController implements OnDetectionStateChangedListener {
     // 로봇의 현재 상태를 나타내는 변수
-    private String currentAngle;
-    private String currentDistance;
-    private int currentOut;
-    private String currentTime;
+    private int angle = 90;
+    private float distance = 0.0F;
+    private boolean out = false;
+    private long time = 0L;
+    private ForwardAction forwardAction;
+    private Robot robot;
+    private Handler handler;
+    Queue<Integer> queue = new LinkedList<>();
+
 
     // 로봇과의 통신을 위한 메서드 및 변수
 
-    public RobotController() {
-        // 초기화 초기설정 입력
+    public RobotController(Handler handler, Robot robot) {
+        this.robot = robot;
+        this.handler = handler;
+        this.forwardAction = new ForwardAction(handler, robot);
+        robot.addOnDetectionStateChangedListener(this);
+        setupFirebaseListeners();
     }
 
-    // 로봇의 움직임을 업데이트하는 메서드
-    public void updateMovement(String angle, String distance, int out, String time) {
-        // 여기에 로봇을 움직이게 하는 로직 구현
-        currentAngle = angle;
-        currentDistance = distance;
-        currentOut = out;
-        currentTime = time;
+    private void setupFirebaseListeners(){
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
-        // 예: SDK를 사용하여 로봇에게 이동 명령을 보내는 코드
+        for (int i = 1; i <= 6; i++){
+            final int robotNumber = i; // 로봇의 각 번호
+            databaseReference.child("robot" + robotNumber).addValueEventListener(new ValueEventListener(){
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot){
+                    try {
+                        String angleString = dataSnapshot.child("angle").getValue(String.class);
+                        String distanceString = dataSnapshot.child("distance").getValue(String.class);
+                        String outString = dataSnapshot.child("out").getValue(String.class);
+                        String timeString = dataSnapshot.child("time").getValue(String.class);
+
+                        int angle = Integer.parseInt(angleString);
+                        float distance = Float.parseFloat(distanceString);
+                        boolean out = Boolean.parseBoolean(outString);
+                        float time = Float.parseFloat(timeString);
+
+
+                        if (out) {
+                            resetPosition(robotNumber); // Reset position for specific robot
+                        } else {
+                            addActionToQueue(robotNumber, angle, distance, time);
+                        }
+                    } catch (NumberFormatException e){
+                        Log.e("RobotController", "Error parsing Firebase data", e);
+                    }
+
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error){
+                    Log.e("Firebase", "DatabaseError: " + error.getMessage());
+                }
+            });
+        }
     }
+
 
     // 로봇의 움직임을 시작하는 메서드
-    public void startMovement() {
-        // 여기에 로봇의 움직임을 시작하는 로직 구현
-        ForwardAction.moveForwardForDuration(10);
+    public void startMovement(int durationMillis) {
+        forwardAction.moveForwardForDuration(durationMillis);
     }
 
-    // 로봇을 초기 위치로 되돌리는 메서드
-    public void resetPosition() {
-        // 여기에 로봇을 초기 위치로 되돌리는 로직 구현
-        String s = "home base";
-
+    public void resetPosition(int robotNumber) {
+        robot.goTo("Home");
     }
 
-    // 현재 로봇 상태를 반환하는 메서드
+    // distance 를 durationMillis로 바꾸는 메소드
+    private int convertDistanceToDurationMillis(float distance){
+        //공식은 바뀔 수 있음. 일단 distance 10은 10초간 이동하는 것으로 대입
+        return (int)(distance * 1000);
+    }
+
+    private void rotateRobot(int robotNumber, int angle){
+       Robot specificRobot = getRobotInstance(robotNumber);
+       if(specificRobot != null){
+           specificRobot.turnBy(angle, 1.0f);
+       }
+    }
+
+    private void addActionToQueue(int robotNumber, int angle, float distance, float time){
+        Map<String, Object> actionData = new HashMap<>();
+        actionData.put("robotNumber", robotNumber);
+        actionData.put("angle", angle);
+        actionData.put("distance", distance);
+        actionData.put("time", time);
+        actionQueue.offer(actionData);
+        processNextAction();
+    }
+
+    private void processNextAction(){
+        if (!actionQueue.isEmpty()) {
+            Map<String, Object> actionData = actionQueue.poll();
+            executeAction(actionData);
+        }
+    }
+    private PriorityQueue<Map<String,Object>> actionQueue = new PriorityQueue<>(
+            Comparator.comparingInt(a -> (Integer) a.get("time"))
+    );
+    private void executeAction(Map<String, Object> actionData){
+        int robotNumber = (Integer) actionData.get("robotNumber");
+        int angle = (Integer) actionData.get("angle");
+        float distance = (Float) actionData.get("distance");
+
+        rotateRobot(robotNumber, angle);
+        forwardAction.moveForwardForDuration(convertDistanceToDurationMillis(distance));
+    }
+
+
+    private Robot getRobotInstance(int robotNumber){
+        switch (robotNumber){
+            case 1://수정중
+        }
+        return null;
+    }
+    @Override
+    public void onDetectionStateChanged(int state){
+        switch(state){
+            case OnDetectionStateChangedListener.DETECTED:
+                robot.stopMovement();
+                robot.turnBy(angle, 1.0F);
+
+                int durationMillis = convertDistanceToDurationMillis(distance);
+                handler.postDelayed(() -> forwardAction.moveForwardForDuration(durationMillis), 5000);
+                break;
+
+            case OnDetectionStateChangedListener.LOST:
+                break;
+            case OnDetectionStateChangedListener.IDLE:
+                break;
+        }
+    }
     public String getCurrentStatus() {
-        return String.format("Angle: %s, Distance: %s, Out: %d, Time: %s", currentAngle, currentDistance, currentOut, currentTime);
+        return String.format("Angle: %d, Distance: %.2f, Out: %b, Time: %d", angle, distance, out, time);
     }
 
     // 기타 필요한 메서드 추가 가능
